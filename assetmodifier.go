3118cd701d58e068d1c95c5c6f1aef676f44e707
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-08-06 00:21:10 +0200

    Added several modifier types

--- assetmodifier.go
+++ assetmodifier.go
@@ -22,11 +22,20 @@ func (am *AssetModifier) initFromData(data *vdf.KeyValue) error {
 }
 
 const (
-	MODIFIER_UNKNOWN             = ""
-	MODIFIER_ENTITY_MODEL        = "entity_model"
-	MODIFIER_PARTICLE            = "particle"
-	MODIFIER_PARTICLE_CREATE     = "particle_create"
-	MODIFIER_PARTICLE_SNAPSHOT   = "particle_snapshot"
-	MODIFIER_ADDITIONAl_WEARABLE = "additional_wearable"
-	MODIFIER_PERSONA             = "persona"
+	MODIFIER_UNKNOWN                   = ""
+	MODIFIER_ENTITY_MODEL              = "entity_model"
+	MODIFIER_PARTICLE                  = "particle"
+	MODIFIER_PARTICLE_CREATE           = "particle_create"
+	MODIFIER_PARTICLE_SNAPSHOT         = "particle_snapshot"
+	MODIFIER_ADDITIONAL_WEARABLE       = "additional_wearable"
+	MODIFIER_PERSONA                   = "persona"
+	MODIFIER_ACTIVITY                  = "activity"
+	MODIFIER_HERO_MODEL_CHANGE         = "hero_model_change"
+	MODIFIER_MODEL                     = "model"
+	MODIFIER_MODEL_SKIN                = "model_skin"
+	MODIFIER_PET                       = "pet"
+	MODIFIER_PORTRAIT_BACKGROUND_MODEL = "portrait_background_model"
+	MODIFIER_COURIER                   = "courier"
+	MODIFIER_COURIER_FLYING            = "courier_flying"
+	MODIFIER_ENTITY_CLIENTSIDE_MODEL   = "entity_clientside_model"
 )
